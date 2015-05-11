bb29817b3fcf00df7e7b463c5e7a4e72e04c4a55
John M Buscemi <jmbuscemi@gmail.com>
2015-05-11 15:45:15 -0400

    Ship place accross/down. Ship can't be placed twice

--- ship.rb
+++ ship.rb
@@ -6,35 +6,28 @@ class Ship
 
   def initialize (length)
     @length = length
-    @array = []
+    @coverage = []
   end
 
   def place(x_loc, y_loc, orientation)
-    @x_start = x_loc
-    @y_start = y_loc
-    if orientation
-      @orientation = "across"
+    if @x_start.nil?
+      @x_start = x_loc
+      @y_start = y_loc
+      if orientation
+        @orientation = "across"
+        (@x_start..(@x_start + @length - 1)).each {|i| @coverage << [i, @y_start]}
+      else
+        @orientation = "down"
+        (@y_start..(@y_start + @length - 1)).each {|i| @coverage << [@x_start, i]}
+      end
+
     else
-      @orientation = "down"
-   end
-  #  @array = []
-  #  if @array.empty?
-  #    @array << x_loc
-  #    @array << y_loc
-
-
+      false
+    end
   end
 
   def covers?(x_loc, y_loc)
-    if @orientation == "across"
-      @x_coverage = (@x_start..(@x_start + @length - 1)).to_a
-      @y_coverage = [@y_start]
-    else
-      @x_coverage = [@x_start]
-      @y_coverage = (@y_start..(@y_start + @length - 1)).to_a
-    end
-
-    if @x_coverage.include?(x_loc) && @y_coverage.include?(y_loc)
+    if @coverage.include?([x_loc, y_loc])
       true
     else
       false
