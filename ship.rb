fcae0f960c6e018ab6c63683135b0ce77026f135
Cruz <nunez.a.cruz@gmail.com>
2015-05-11 14:44:31 -0400

    Test 8 and 9 w/ hash

--- ship.rb
+++ ship.rb
@@ -2,19 +2,21 @@ class Ship
   attr_reader :length
   def initialize(length)
     @length = length
-    board = {}
+    @board = {}
+  end
+  def covers?(column, row)
+     @board[column][row]
   end
 
-  def covers?(column, row)
-    unless (column == 1 && row = 1) || (column == 6 && row = 1) ||
-      (column == 4 && row == 2)
+  def place(column, row, direction)
+    (1..10).each {|n| @board[n] = Hash.new(false)}
+    if direction
+      (2..5).each {|a| @board[a][1] = true}
+    return true
+    else
+      (2..5).each {|a| @board[2][a] = true}
       return true
     end
-  end
 
-  def place(column, row, covered)
-    if covered
-      return true
-    end  
   end
 end
