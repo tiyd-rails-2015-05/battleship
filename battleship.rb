575295280f71405bcb5533b8e94deb437a0663f6
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-05-11 16:21:41 -0400

    Complete tests 8 and 9

--- battleship.rb
+++ battleship.rb
@@ -30,5 +30,24 @@ class Ship
   attr_reader :length
   def initialize(length)
     @length = length
+    @placed_ship = []
+  end
+
+  def place (x, y, bool)
+    if bool == true
+      (x..(x + @length - 1)).each do |i|
+        @placed_ship << [i, y]
+      end    #[i] = y
+    else
+      (y..(y + @length - 1)).each do |i|
+        @placed_ship << [x, i]
+      
+      end
+    end
+
+  end
+
+  def covers? (x, y)
+    @placed_ship.include?([x,y])
   end
 end
