c6ca4c974ea5d98c6b5597509000e989238d0c67
Anna Bedell <awbedell@gmail.com>
2015-05-11 16:55:33 -0400

    Tests 1-23 working

--- grid.rb
+++ grid.rb
@@ -3,6 +3,7 @@ require './ship.rb'
 class Grid
   def initialize
     @locations = []
+    @hits = []
   end
 
   def place_ship(ship, x, y, horizontal)
@@ -19,43 +20,31 @@ class Grid
     @locations.include?([x, y])
   end
 
-  def empty_grid
-    puts     %Q{    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A |   |   |   |   |   |   |   |   |   |   |
-B |   |   |   |   |   |   |   |   |   |   |
-C |   |   |   |   |   |   |   |   |   |   |
-D |   |   |   |   |   |   |   |   |   |   |
-E |   |   |   |   |   |   |   |   |   |   |
-F |   |   |   |   |   |   |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   |   |   |   |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------
-}
-  end
-  
-  def ready_grid
-    %Q{    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A | O | O | O | O |   |   |   |   |   |   |
-B |   |   |   |   |   | O |   |   |   |   |
-C |   |   |   |   |   | O |   |   |   |   |
-D |   |   |   |   |   | O | O | O | O |   |
-E |   |   |   |   |   | O |   |   |   |   |
-F |   |   | O | O |   | O |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   | O | O | O |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------
-}
-  end
-
   def display
-    empty_grid
+    puts %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------}
+    letter = ('A'..'J').to_a
+    (1..10).each do |row|
+      draw_row = "#{letter[row-1]} "
+        (1..10).each do |column|
+          if @locations.include?([column, row])
+            draw_row += "| O "
+          elsif @hits.include?([column, row])
+            draw_row += "| X "
+          else
+            draw_row += "|   "
+          end
+        end
+        draw_row += "|"
+        puts draw_row
+    end
+    puts "  -----------------------------------------"
   end
 
+  def fire_at(x, y)
+    if @locations.include?([x, y]) && !@hits.include?([x, y])
+      @hits += [x, y]
+    end
+  end
 
 end
