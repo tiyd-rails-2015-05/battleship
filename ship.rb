3573c39507ebc0d1402eabdc0361bab81bea4f86
Joe Reiff <jcreiff@gmail.com>
2015-05-11 19:47:06 -0400

    test 13 passes, create Hole class and sunk? method

--- ship.rb
+++ ship.rb
@@ -1,10 +1,12 @@
 class Ship
-  attr_reader :length, :place, :ship_locations
+  require './hole.rb'
+  attr_reader :length, :place, :ship_locations, :hole_array
 
   def initialize(length)
     @length = length
     @place = false
     @ship_locations = []
+    @hole_array = []
   end
 
   def place(x, y, orientation=true)
@@ -14,11 +16,13 @@ class Ship
       y_start = @place[1]
       if orientation == true
         (x_start..x_start + @length-1).each do |x|
+          @hole_array << Hole.new(x, y_start)
           location = [x, y_start]
           @ship_locations << location
         end
       else
         (y_start..y_start + @length -1).each do |y|
+          @hole_array << Hole.new(x_start, y)
           location = [x_start, y]
           @ship_locations << location
         end
@@ -35,8 +39,35 @@ class Ship
   end
 
   def fire_at(x, y)
-    @ship_locations.include?([x, y])
+    if @ship_locations.include?([x, y])
+      @hole_array.each do |hole|
+          hole.hit?(x, y)
+      end
+    end
   end
 
+  def sunk?
+    check_sunk = []
+    @hole_array.each do |hole|
+      check_sunk << hole.hit
+      end
+    if check_sunk.include?(false)
+      return false
+    else
+      return true
+    end
+  end
 
 end
+# un-comment to see the result of running test 13
+# ship = Ship.new(2)
+# ship.place(2, 1, true)
+# p ship.hole_array
+#
+#
+# puts ship.sunk?
+# ship.fire_at(2, 1)
+# puts ship.sunk?
+# ship.fire_at(3, 1)
+# puts ship.sunk?
+# p ship.hole_array
