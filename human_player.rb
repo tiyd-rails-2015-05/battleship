d8a4eaba1129052118d1628f778a3395859f6a96
Anna Bedell <awbedell@gmail.com>
2015-05-11 19:53:38 -0400

    Tests 1-30 working

--- human_player.rb
+++ human_player.rb
@@ -19,16 +19,21 @@ class HumanPlayer < Player
     ships_to_place.each do |ship|
       new_ship = Ship.new(ship)
       until new_ship.placed
-        puts "#{name}, where would you like to place a ship of length #{ship}?"
+        puts "#{@name}, where would you like to place a ship of length #{ship}?"
         coordinate = get_user_input
         puts "Across or Down?"
         orientation = get_user_input
-        p orientation
-        orientation = false if orientation.downcase == "Down"
-        @ships.each do |test_ship|
-          if new_ship.overlaps_with?(test_ship)
-            puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+        if orientation == "Down"
+          orientation = false
+        end
+        if !@ships.empty?
+          @ships.each do |test_ship|
+            if test_ship.overlaps_with?(new_ship)
+              puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+            end
           end
+        else
+          new_ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
         end
       end
       @grid.place_ship(new_ship, @grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
