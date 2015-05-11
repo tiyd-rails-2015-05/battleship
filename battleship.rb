96619d94d7e152b32ff1c5274144f5cc6057cbeb
Scott Williams <williamsjs1224@gmail.com>
2015-05-11 17:00:22 -0400

    add function up to ship_sunk?

--- battleship.rb
+++ battleship.rb
@@ -28,19 +28,21 @@ class ComputerPlayer < Player
 end
 
 class Ship
-  attr_reader :length
+  attr_reader :length, :locations
 
   def initialize(length)
     @length = length
   end
 
   def place(x_val, y_val, bool)
-    @locations = []
-    length.times do |count|
-      if bool
-        @locations.push([x_val+count, y_val])
-      else
-        @locations.push([x_val, y_val+count])
+    if @locations.nil?
+      @locations = []
+      length.times do |count|
+        if bool
+          @locations.push([x_val+count, y_val])
+        else
+          @locations.push([x_val, y_val+count])
+        end
       end
     end
   end
@@ -49,8 +51,37 @@ class Ship
     @locations.include?([x_val, y_val])
   end
 
+  def overlaps_with?(ship_object)
+    @locations.each do |item|
+      x = item[0]
+      y = item[1]
+      if ship_object.covers?(x, y)
+        return true
+      end
+    end
+    false
+  end
+
+  def fire_at(x, y)
+    if @locations.include?([x, y])
+      return true
+    end
+  end
+
+  def ship_sunk?
+
+  end
+
 end
 
-my_ship = Ship.new(4)
-my_ship.place(2, 1, true)
-my_ship.covers?(4, 1)
+# ship1 = Ship.new(4)
+# ship1.place(2, 1, true)
+# ship2 = Ship.new(4)
+# ship2.place(3, 1, true)
+# ship3 = Ship.new(4)
+# ship3.place(2, 1, false)
+#
+# # Try to use your `covers?` method inside your `overlaps_with?` code.
+# puts ship1.overlaps_with?(ship2)
+# puts ship1.overlaps_with?(ship3)
+# puts ship2.overlaps_with?(ship3)
