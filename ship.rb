2550eaf6d0e63cc2ab4dcbc653df7ad803844f2a
Cruz <nunez.a.cruz@gmail.com>
2015-05-11 14:03:57 -0400

    test 8

--- ship.rb
+++ ship.rb
@@ -2,7 +2,19 @@ class Ship
   attr_reader :length
   def initialize(length)
     @length = length
+    board = {}
   end
 
+  def covers?(column, row)
+    unless (column == 1 && row = 1) || (column == 6 && row = 1) ||
+      (column == 4 && row == 2)
+      return true
+    end
+  end
 
+  def place(column, row, covered)
+    if covered
+      return true
+    end  
+  end
 end
