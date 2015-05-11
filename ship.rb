a85552fa250e2f86666aed39b8e3dd39e5cc646a
Joe Reiff <jcreiff@gmail.com>
2015-05-11 17:14:32 -0400

    move ship_locations instance variable from covers? method into place method

--- ship.rb
+++ ship.rb
@@ -4,31 +4,34 @@ class Ship
   def initialize(length)
     @length = length
     @place = false
+    @ship_locations = []
   end
 
   def place(column, row, orientation=true)
     if @place == false
       @place = [column, row, orientation]
+      column_start = @place[0]
+      row_start = @place[1]
+      if orientation == true
+        (column_start..column_start + @length-1).each do |x|
+          location = [x, row_start]
+          @ship_locations << location
+        end
+      else
+        (row_start..row_start + @length -1).each do |x|
+          location = [column_start, x]
+          @ship_locations << location
+        end
+      end
     end
   end
 
-  def covers?(column, row)
-    spots = []
-    column_start = @place[0]
-    row_start = @place[1]
 
-    if @place[2]
-      (column_start..column_start + @length-1).each do |x|
-        spot = [x, row_start]
-        spots << spot
-      end
-    else
-      (row_start..row_start + @length -1).each do |x|
-        spot = [column_start, x]
-        spots << spot
-      end
-    end
-    spots.include?([column, row])
+  def covers?(column, row)
+    @ship_locations.include?([column, row])
+  end
+
+  def overlaps_with?(other_ship)
   end
 
 
@@ -38,10 +41,27 @@ end
 
 # ship = Ship.new(4)
 # ship.place(2, 1)
-# puts ship.covers?(2, 1)
+#p ship.ship_locations
+# p @ship_locations
 
 # if @orientation
 #   columns = (column..column + @length-1)
 # else
 #   rows = (row..row + @length-1)
+# end
+# def ship_locations
+#
+#
+#   if @place
+#     (column_start..column_start + @length-1).each do |x|
+#       location = [x, row_start]
+#       @ship_locations << location
+#     end
+#   else
+#     (row_start..row_start + @length -1).each do |x|
+#       location = [column_start, x]
+#       @ship_locations << location
+#     end
+#   end
+#   # @ship_locations
 # end
