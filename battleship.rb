7c4a3d6454ce235c5e4e2e5d60b853d106eefe5d
Joe Reiff <jcreiff@gmail.com>
2015-05-11 13:22:56 -0400

    test 5 passes, test 4 now does not

--- battleship.rb
+++ battleship.rb
@@ -5,11 +5,18 @@ class HumanPlayer < Player
 
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name = "Dave")
     @name = name
   end
-  
+
 end
 
 class ComputerPlayer < Player
+
+  attr_reader :name
+
+  def initialize(name = "HAL 9000")
+    @name = name
+  end
+  
 end
