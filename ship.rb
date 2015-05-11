33f1d3796b2f72e2547b9cde67bcc7a0117a8dd7
Anna Bedell <awbedell@gmail.com>
2015-05-11 13:10:03 -0400

    Initial Commit

---
+++ ship.rb
@@ -0,0 +1,27 @@
+class Ship
+  def initialize(x, y, horizontal)
+    @x = x
+    @y = y
+    @horizontal = horizontal
+  end
+
+  def length
+    4
+  end
+
+  def covers?
+
+  end
+
+  def place
+
+  end
+
+  def overlaps_with?
+
+  end
+
+  def fire_at
+
+  end
+end
