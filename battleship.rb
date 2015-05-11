97c0ecf705c0d62817e9586fe2cbdf1f81232ddc
Joe Reiff <jcreiff@gmail.com>
2015-05-11 13:45:26 -0400

    split classes into separate files, tests 1-7 still pass

--- battleship.rb
+++ battleship.rb
@@ -1,32 +1,4 @@
-class Player
-end
-
-class HumanPlayer < Player
-
-  attr_reader :name
-
-  def initialize(name = "Dave")
-    @name = name
-  end
-
-end
-
-class ComputerPlayer < Player
-
-  attr_reader :name
-
-  def initialize
-    @name = "HAL 9000"
-  end
-
-end
-
-class Ship
-
-  attr_reader :length
-
-  def initialize(length)
-    @length = length
-  end
-  
-end
+require './player.rb'
+require './human_player.rb'
+require './computer_player.rb'
+require './ship.rb'
---
+++ human_player.rb
@@ -0,0 +1,7 @@
+class HumanPlayer < Player
+  attr_reader :name
+
+  def initialize(name = "Dave")
+    @name = name
+  end
+end
