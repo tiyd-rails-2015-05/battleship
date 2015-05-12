28579a5969848e600cce1828e5aac63d0c95de98
Anna Bedell <awbedell@gmail.com>
2015-05-12 08:52:05 -0400

    #31 modification, still not working

--- human_player.rb
+++ human_player.rb
@@ -18,26 +18,36 @@ class HumanPlayer < Player
   def place_ships(ships_to_place)
     ships_to_place.each do |ship|
       new_ship = Ship.new(ship)
-      until new_ship.placed
-        puts "#{@name}, where would you like to place a ship of length #{ship}?"
-        coordinate = get_user_input
-        puts "Across or Down?"
-        orientation = get_user_input
-        if orientation == "Down"
-          orientation = false
-        end
-        if !@ships.empty?
-          @ships.each do |test_ship|
-            if test_ship.overlaps_with?(new_ship)
-              puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
-            end
-          end
-        else
-          new_ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
-        end
+      puts "#{@name}, where would you like to place a ship of length #{ship}?"
+      coordinate = get_user_input
+      puts "Across or Down?"
+      orientation = get_user_input
+      if orientation == "Across"
+        horizontal = true
       end
-      @grid.place_ship(new_ship, @grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
+      new_ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), horizontal)
+      puts new_ship.length
+      p new_ship.location
+      if !@ships.empty?
+        @ships.each do |test_ship|
+          until !test_ship.overlaps_with?(new_ship)
+            p "here"
+            break
+            puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+            puts "#{@name}, where would you like to place a ship of length #{ship}?"
+            coordinate = get_user_input
+            puts "Across or Down?"
+            orientation = get_user_input
+            horizontal = true if orientation == "Across"
+            new_ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), horizontal)
+          end
+          @grid.place_ship(new_ship, @grid.x_of(coordinate), @grid.y_of(coordinate), horizontal)
+        end
+      else
+        @grid.place_ship(new_ship, @grid.x_of(coordinate), @grid.y_of(coordinate), horizontal)
+      end
       @ships << new_ship
+      puts @ships
     end
   end
 
