c61ca3fdaeeb2fe5b497a00031540b3ec0f729f1
John M Buscemi <jmbuscemi@gmail.com>
2015-05-12 18:54:00 -0400

    Create grid as array. Grid display will show placed ships

--- grid.rb
+++ grid.rb
@@ -1,9 +1,26 @@
 
 class Grid
+  #include FireAt
   attr_reader :ships
 
   def initialize
     @ships = []
+    @hits = []
+    @misses = []
+    @board = {
+    	A: [false, false, false, false, false, false, false, false, false, false],
+    	B: [false, false, false, false, false, false, false, false, false, false],
+    	C: [false, false, false, false, false, false, false, false, false, false],
+    	D: [false, false, false, false, false, false, false, false, false, false],
+    	E: [false, false, false, false, false, false, false, false, false, false],
+    	F: [false, false, false, false, false, false, false, false, false, false],
+    	G: [false, false, false, false, false, false, false, false, false, false],
+    	H: [false, false, false, false, false, false, false, false, false, false],
+    	I: [false, false, false, false, false, false, false, false, false, false],
+    	J: [false, false, false, false, false, false, false, false, false, false]
+    }
+    @convert_hash = {1 => :A, 2 => :B, 3 => :C, 4 => :D, 5 => :E, 6 => :F, 7 => :G,
+    								8 => :H, 9 => :I, 10 => :J}
   end
 
   def has_ship_on?(x_loc, y_loc)
@@ -22,61 +39,81 @@ class Grid
   end
 
   def empty_grid
-  puts  %Q{    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A |   |   |   |   |   |   |   |   |   |   |
-B |   |   |   |   |   |   |   |   |   |   |
-C |   |   |   |   |   |   |   |   |   |   |
-D |   |   |   |   |   |   |   |   |   |   |
-E |   |   |   |   |   |   |   |   |   |   |
-F |   |   |   |   |   |   |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   |   |   |   |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------
-}
+  puts %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------}
+
+  @board.each do |key, row|
+  	string = ""
+  	row.each do |v|
+  		if v
+  			string += " O |"
+  		else
+  			string += "   |"
+  		end
+  	end
+
+  	puts key.to_s + " |" + string
+  end
+  puts %Q{  -----------------------------------------}
   end
 
   def place_ship(ship, x_loc, y_loc, orientation)
     ship.place(x_loc, y_loc, orientation)
-    @overlapping = false
+    overlapping = false
     @ships.each do |s|
-      @overlapping = true if s.overlaps_with?(ship)
+      overlapping = true if s.overlaps_with?(ship)
     end
     #something using each which sets overlapping
-    if @overlapping
+    if overlapping
       false
     else
       @ships << ship
+      if orientation  #Across
+        ship.length.times do
+          row = @convert_hash[y_loc]
+          @board[row][x_loc - 1] = true
+          x_loc += 1
+        end
+      else  #Down
+        ship.length.times do
+          row = @convert_hash[y_loc]
+          @board[row][x_loc - 1] = true
+          y_loc += 1
+        end
+      end
       true
     end
+
   end
 
   def ready_grid
-  puts  %Q{    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A | O | O | O | O |   |   |   |   |   |   |
-B |   |   |   |   |   | O |   |   |   |   |
-C |   |   |   |   |   | O |   |   |   |   |
-D |   |   |   |   |   | O | O | O | O |   |
-E |   |   |   |   |   | O |   |   |   |   |
-F |   |   | O | O |   | O |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   | O | O | O |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------
-}
+  puts %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------}
+  @board.each do |key, row|
+  	string = ""
+  	row.each do |v|
+  		if v
+  			string += " O |"
+  		else
+  			string += "   |"
+  		end
+  	end
+  	puts key.to_s + " |" + string
+  end
+  puts %Q{  -----------------------------------------}
   end
 
   def fire_at(x_loc, y_loc)
     if @ships.empty? || x_loc > 10 || y_loc > 10
       false
-    elsif @ships.include?([x_loc, y_loc])
+    elsif has_ship_on?(x_loc, y_loc)
+      @hits << [x_loc, y_loc]
+      true
+    else
+      @misses << [x_loc, y_loc]
+      false
     end
   end
-
 
 
 
