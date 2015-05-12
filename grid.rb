0f9be8017407ea8c666588425169192f540e489f
Joe Reiff <jcreiff@gmail.com>
2015-05-11 23:09:23 -0400

    test 17 passes...i think?

--- grid.rb
+++ grid.rb
@@ -1,9 +1,33 @@
 class Grid
 
+attr_reader :display, :empty_grid
+
   def initialize
+    @display = display
   end
 
   def has_ship_on?(x, y)
+  end
+
+  def display
+    puts empty_grid
+  end
+
+  def empty_grid
+    %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A |   |   |   |   |   |   |   |   |   |   |
+B |   |   |   |   |   |   |   |   |   |   |
+C |   |   |   |   |   |   |   |   |   |   |
+D |   |   |   |   |   |   |   |   |   |   |
+E |   |   |   |   |   |   |   |   |   |   |
+F |   |   |   |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   |   |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+}
   end
 
 end
