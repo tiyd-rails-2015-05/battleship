77f8d929897cfba325bc3e754976790cf1548a46
Anna Bedell <awbedell@gmail.com>
2015-05-12 10:20:44 -0400

    Game class

---
+++ game.rb
@@ -0,0 +1,20 @@
+class Game
+
+  def initialize(first_player, second_player)
+    @first_player = first_player
+    @second_player = second_player
+  end
+
+  def welcome
+    puts "Welcome, #{@first_player.name} and #{@second_player.name}!\nIt's time to play Battleship.\n"
+  end
+
+  def place_ships
+    @first_player.place_ships
+    @second_player.place_ships
+  end
+
+  def display_status
+
+  end
+end
