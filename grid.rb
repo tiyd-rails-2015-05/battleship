426ddb29257dc42c43311f236d0975ede70cf3d5
Joe Reiff <jcreiff@gmail.com>
2015-05-12 12:52:27 -0400

    test 18 pass. VICTORY.

--- grid.rb
+++ grid.rb
@@ -1,12 +1,22 @@
 class Grid
 
+require './ship.rb'
+require './hole.rb'
 attr_reader :display, :empty_grid
 
   def initialize
     @display = display
+    @ships = []
   end
 
   def has_ship_on?(x, y)
+    on = false
+    @ships.each do |ship|
+      if ship.covers?(x, y)
+        on = true
+      end
+    end
+    return on
   end
 
   def display
@@ -29,5 +39,19 @@ J |   |   |   |   |   |   |   |   |   |   |
   -----------------------------------------
 }
   end
+
+  def place_ship(ship, x, y, orientation=true)
+    ship.place(x, y, orientation)
+    @ships<<ship
+  end
+
+  grid = Grid.new
+  grid.place_ship(Ship.new(4), 3, 3, true)
+  grid.has_ship_on?(2, 3)
+  grid.has_ship_on?(3, 3)
+  grid.has_ship_on?(4, 3)
+  grid.has_ship_on?(6, 3)
+  grid.has_ship_on?(7, 3)
+  grid.has_ship_on?(5, 4)
 
 end
