b6589b627a3e67c1f7b9d5a9c3082fca1b690eb3
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-11 20:43:44 -0400

    pass test 11

--- battleship.rb
+++ battleship.rb
@@ -1,39 +1,34 @@
 require './battleship_test.rb'
 
 class Player
-
 end
 
 
 
 class HumanPlayer < Player
 attr_reader :name
-
   def initialize(name = "Dave")
     @name = name
   end
-
 end
 
 
 
 class ComputerPlayer < Player
 attr_reader :name
-
   def initialize
     @name = "HAL 9000"
   end
-
 end
 
 class Ship
-  attr_reader :length
+  attr_reader :length, :placed_ship
   def initialize(length)
     @length = length
     @placed_ship = []
   end
 
-  def place (x, y, bool)
+  def place(x, y, bool)
     if @placed_ship == []
       if bool == true
         (x..(x + @length - 1)).each do |i|
@@ -47,7 +42,19 @@ class Ship
     end
   end
 
-  def covers? (x, y)
+  def covers?(x, y)
     @placed_ship.include?([x,y])
   end
+
+  def overlaps_with?(check_ship)
+    @placed_ship.length.times.each do |i|
+        if  check_ship.covers?(@placed_ship[i-1][0],@placed_ship[i-1][1]) == true
+          return true
+          exit loop
+        end
+
+    end
+    return false
+  end
+
 end
