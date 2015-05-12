1048f5a62a957d55fb6e6c2caf613a453d4e27f9
Cruz <nunez.a.cruz@gmail.com>
2015-05-12 14:26:46 -0400

    Test 20

--- grid.rb
+++ grid.rb
@@ -1,123 +1,79 @@
 class Grid
-  attr_reader :board
-  def initialize
-    @board = {}
-    (1..10).each do |row|
-      @board[row] = Hash.new(1..10)
-      (1..10).each do |column|
-        @board[row][column] = false
-      end
-    end
+ def initialize
+   @board = {}
+   (1..10).each {|n| @board[n] = Hash.new(false)}
+   (1..10).each {|n| (1..10).each {|a| @board[n][a] = false}}
+ end
 
-  end
+ def has_ship_on?(column, row)
+   @board[column][row]
+ end
 
-  def has_ship_on?(column, row)
-    @board[row][column]
-  end
+#  def has_ship_on?(column, row)
+#    false
+#  end
 
-  def display
-#     puts(
-# "    1   2   3   4   5   6   7   8   9   10
-#   -----------------------------------------
-# A |   |   |   |   |   |   |   |   |   |   |
-# B |   |   |   |   |   |   |   |   |   |   |
-# C |   |   |   |   |   |   |   |   |   |   |
-# D |   |   |   |   |   |   |   |   |   |   |
-# E |   |   |   |   |   |   |   |   |   |   |
-# F |   |   |   |   |   |   |   |   |   |   |
-# G |   |   |   |   |   |   |   |   |   |   |
-# H |   |   |   |   |   |   |   |   |   |   |
-# I |   |   |   |   |   |   |   |   |   |   |
-# J |   |   |   |   |   |   |   |   |   |   |
-#   -----------------------------------------"
-# )
-
+def display
+  #print @board
     print " "
-    (1..10).each do |x|
-      print "   #{x}"
-    end
-    print "\n"
-    print "  "
-    (1..41).each{|x| print "-"}
-    print "\n"
-
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    (0..9).each do |index|
-      print "#{alpha[index]} |"
+  (1..10).each do |x|
+    print "   #{x}"
+  end
+  print "\n"
+  print "  "
+  (1..41).each{|x| print "-"}
+  print "\n"
+  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  (0..9).each do |index|
+    print "#{alpha[index]} |"
       (0..9).each do |x|
-        print "   |"
+        print " "
+      if @board[x+1][index+1]
+        print "O"
+      else
+        print " "
       end
-      print "\n"
+      print " "
+      print "|"
+       #print "   |"
     end
-    print "  "
-    (1..41).each{|x| print "-"}
     print "\n"
   end
+  print "  "
+  (1..41).each{|x| print "-"}
+  print "\n"
+end
 
-  private def ship_overlap?
-    if direction
-      (column...column+ship.length).each do |a|
-        if @board[row][column] == ship.covers?(column, row)
-          false
-        end
-      end
-    else
-      (row...row+ship.length).each do |b|
-        if @board[row][column] == ship.covers?(column, row)
-          false
-        end
-      end
-    end
-  end
+ def place_ship(ship, column, row, direction)
+   hasship = false
+   if direction
+     (column...column+ship.length).each do |a|
+       if self.has_ship_on?(a, row)
+         hasship = true
+         return false
+       end
+     end
+     if hasship == false
+       (column...column+ship.length).each do |a|
+         @board[a][row] = true
+       end
+     end
+   else
+     (row...row+ship.length).each do |a|
+       if self.has_ship_on?(column, a)
+         hasship = true
+         return false
+       end
 
-  def place_ship(ship, column, row, direction)
-    if direction
-      (column...(column+ship.length)).each do |a|
-        # puts "ship.covers?(a, row): #{ship.covers?(a, row)}"        #for debugging purposes
-        if @board[row][a]
-          return false
-        end
-      end
-      (column...column+ship.length).each do |b|
-        @board[row][b] = true
-      end
-      return true
-    else
-      (row...row+ship.length).each do |c|
-        if @board[c][column]
-          return false
-        end
-      end
-      (row...row+ship.length).each do |d|
-        @board[d][column] = true
-      end
-      return true
-    end
+     end
+     if hasship == false
+       (row...row+ship.length).each do |a|
+         @board[column][a] = true
+       end
+     end
+   end
+   return true
+ end
 
-    # if @board[row][column] == ship.covers?(column, row)
-    #
-    #   if direction
-    #
-    #     (column...column+ship.length).each do |a|
-    #       @board[row][a] = true
-    #     end
-    #
-    #   else
-    #
-    #     (row...row+ship.length).each do |a|
-    #       @board[a][column] = true
-    #     end
-    #
-    #   end
-    #
-    # else
-    #   false
-    # end
-
-
-
-
-  end
 
 end
