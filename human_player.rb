f32f2725a80404ed34daecdb96a1e9c51baf9a56
Aaron Wiggins <apw7177@gmail.com>
2015-05-13 20:38:59 -0400

    human player cannot overlap ships

--- human_player.rb
+++ human_player.rb
@@ -14,23 +14,28 @@ class HumanPlayer < Player
 
   def place_ships(array)
     array.each do |i|
-      ship = Ship.new(i)
-      puts "#{@name}, where would you like to place a ship of length #{i}?"
-      coordinate = get_user_input
-      puts "Across or Down?"
-      orientation = get_user_input
+      placed = false
+      until placed == true
+        ship = Ship.new(i)
+        puts "#{@name}, where would you like to place a ship of length #{i}?"
+        coordinate = get_user_input
+        puts "Across or Down?"
+        orientation = get_user_input
+        if orientation == "Across"
+          orientation = true
+        else
+          orientation = false
+        end
+        ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
+        if ship.coverage.any? {|x| grid.has_ship_on?(x[0],x[1])}
+          puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again.\n"
+        else
+          @grid.place_ship(ship,@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
+          @ships << ship
+          placed = true
 
-      # if orientation == "Across"
-      #    orientation = true
-      # else
-      #   orientation = false
-      # end
-      if orientation == "Across" ? true : false
-      ship.place(@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
-      @grid.place_ship(ship,@grid.x_of(coordinate), @grid.y_of(coordinate), orientation)
-      @ships << ship
-      # p @ships
-      # p @grid
+        end
+      end
     end
   end
 end
